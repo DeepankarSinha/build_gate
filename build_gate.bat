91649e264551e936eb92cedbede2a0f6314f2f88
Deepankar Sinha <deepankar@promactinfo.com>
2019-04-29 13:08:45 +0530

    First Working Version

--- build_gate.bat
+++ build_gate.bat
@@ -9,11 +9,17 @@ echo   /    )   /   /  /   /   /   /       /  --,   /   ) /    /___)
 echo _/____/___(___(__/___/___(___/_______(____/___(___(_(_ __(___ _
 echo  V: 0.0.1
 echo  For this to work the head commit must be a merge commit!
+echo.
 
 ::Configuration
-set WHEN_I_FIND=".ts" 
+set PRE_EXECUTE=echo HELLO
+set POST_EXECUTE=echo HELLO END
 
-set I_EXECUTE[0]= "echo Found ts"
+::Be careful to increment the array index correctly
+set WHEN_I_FIND[0]=.ts
+set I_EXECUTE[0]=echo Found ts
+set WHEN_I_FIND[1]=.cs
+set I_EXECUTE[1]=echo Found cs
 ::End Configuration
 
 
@@ -35,14 +41,15 @@ set %~3=%f:~1%
 Exit /b 0
 
 :Search
-Echo."%~1" | findstr "%~2">nul && (
-    echo Echo."%~1" "%~2"
+echo.%~1 | findstr /IC:"%~2">nul && (
     set %~3=TRUE
+) || ( 
+    set %~3=FALSE 
 )
 Exit /b 0
 
 :Run
-echo CMD:%~1
+echo Executing: %~1
 %~1
 Exit /b 0
 
@@ -50,28 +57,63 @@ GOTO :End
 
 :Main 
 
+::Run pre execute if defined
+if defined PRE_EXECUTE (
+    echo Running pre execute command
+    Call :Run "%PRE_EXECUTE%"
+)
+
 Call :SetLastCommitHash commitHash
-echo Hash: %commitHash%
+::echo Hash: %commitHash%
 
 Call :SetMergeCommitRange hash1 hash2
-echo Hash1: %hash1%
-echo Hash2: %hash2%
+::echo Hash1: %hash1%
+::echo Hash2: %hash2%
+if not defined hash2 ( GOTO :NotAMergeCommitError )
+echo Merge commit found^^!
 
 Call :SetFilesModified %hash2% %hash1% files
 echo Files: %files%
 
 set /a count=0
-for /f %%a in (%WHEN_I_FIND%) do (
-    echo FIND: %%a
-    for /f "delims=," %%b in ("%files%") do (
-        echo FILE: %%b
-        Call :Search "%%b" "%%a" flg
-        echo FLAG: !flg!
-        call echo EXE: %count% %%I_EXECUTE[%count%]%%
-        if defined flg ( Call :Run %%I_EXECUTE[%count%]%% )
+set /a found=0
+
+:FindLoop
+if defined WHEN_I_FIND[%count%] (
+    Call set a=%%WHEN_I_FIND[%count%]%%
+
+    Call :Search "%files%" "%%a%%" flag
+
+    if !flag!==TRUE ( 
+        Call :Run "%%I_EXECUTE[%count%]%%"
+        Call set found=found+1
     )
 
-    set /a count+=1
+    if %COUNT%==2 ( 
+        GOTO :End 
+    )
+
+    set /a "count+=1"
+    GOTO :FindLoop
 )
 
+if %found%==0 (
+    echo No files matched the criteria.
+)
+
+::Run post execute if defined
+if defined POST_EXECUTE (
+    echo Running post execute command
+    Call :Run "%POST_EXECUTE%"
+)
+
+GOTO :End
+
+
+:NotAMergeCommitError
+echo Head commit is not a merge commit.
+GOTO :End
+
 :End
+endlocal
+echo My work is done, bye^!
